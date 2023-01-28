27cdf84cbd8a90f6c41aa545027137cfdce30df3
heejin <dlwlrgkwk82@gmail.com>
2023-01-28 12:43:29 +0900

    refactor: use transform for target game

--- small-projects/target/main.js
+++ small-projects/target/main.js
@@ -3,27 +3,37 @@ const verticalLine = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-let toggle = false;
+addEventListener('load', () => { // boundingClientRect value should get after 'target' element is loaded, if not, sometimes, target.getBoundingClientRect().width might be 0
+    let toggle = false;
 
-function moveLineToTarget(e){
-    const x = e.clientX;
-    const y = e.clientY;
-    verticalLine.style.left = `${x}px`;
-    horizontalLine.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-    tag.innerHTML = `${x}px ${y}px`;
-}
+    const targetRect = target.getBoundingClientRect();
+    const targetHalWidth = targetRect.width / 2;
+    const targetHalHeight = targetRect.height / 2;
 
-target.addEventListener('click', () => {
-    if(toggle){
-        target.style.filter = 'opacity(.7) drop-shadow(0 0 0 red)';
-        document.removeEventListener('mousemove', moveLineToTarget);
-    }else{
-        target.style.filter = null;
-        document.addEventListener('mousemove', moveLineToTarget);
+    function moveLineToTarget(e) {
+      const x = e.clientX;
+      const y = e.clientY;
+
+      verticalLine.style.transform = `translateX(${x}px)`;
+      horizontalLine.style.transform = `translateY(${y}px)`;
+
+      target.style.transform = `translate(${x - targetHalWidth}px, ${
+        y - targetHalHeight
+      }px)`;
+
+      tag.style.transform = `translate(${x}px, ${y}px)`;
+
+      tag.innerHTML = `${x}px ${y}px`;
     }
-    toggle = !toggle;
-}); 
+
+    target.addEventListener("click", () => {
+      if (toggle) {
+        target.style.filter = "opacity(.7) drop-shadow(0 0 0 red)";
+        document.removeEventListener("mousemove", moveLineToTarget);
+      } else {
+        target.style.filter = null;
+        document.addEventListener("mousemove", moveLineToTarget);
+      }
+      toggle = !toggle;
+    }); 
+});
